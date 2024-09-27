23e77fc6f950b7826bf7f5da4993930cd8a902c6
Sukhlal  Dawar <144549389+Sukhlald@users.noreply.github.com>
2024-09-27 16:17:18 +0000

    data type created

---
+++ .devcontainer/basis/variable.js
@@ -0,0 +1,10 @@
+const accountId=40925634;
+let accountEmail="abc@gmail.com";
+var password = "12342";
+accountCity ="Seagon";
+console.log(accountId);
+
+//console table
+console.table([accountId,accountEmail,password,accountCity])
+
+//less use of var dataType
